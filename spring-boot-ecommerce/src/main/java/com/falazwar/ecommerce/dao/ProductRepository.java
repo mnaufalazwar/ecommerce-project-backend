6578ce17f68f1e46e822d9d8a9e3e7f3ac806abd
naufal.azwar <naufal.azwar@traveloka.com>
2023-09-26 00:46:54 +0700

    first commit for search product by category

--- spring-boot-ecommerce/src/main/java/com/falazwar/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/falazwar/ecommerce/dao/ProductRepository.java
@@ -1,13 +1,22 @@
 package com.falazwar.ecommerce.dao;
 
 import com.falazwar.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 // this will expose rest api http://localhost:8080/api/products{?page,size,sort}
 // Spring Data REST will expose ALL HTTP method (POST, GET, GET(plural), PUT, DELETE)
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  // Behind the scenes, Spring will execute a query similar to this :
+  // SELECT * FROM product where category_id=?
+  // Spring DATA REST automatically exposes endpoint
+  // http://localhost:8080/api/products/search/findByCategoryId?id=1
+  Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
 
 /*
